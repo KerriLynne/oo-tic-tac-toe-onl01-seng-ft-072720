9260d65789cd83802845a5fe284acddb44bc6da8
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-06 18:42:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,10 @@
 class TicTacToe
 
-  WIN_COMBINATIONS = []
-
   def initialize(board = nil)
   @board = board || Array.new(9, " ")
   end
+
+    WIN_COMBINATIONS = []
 
   # def self.all
   # WIN_COMBINATIONS
