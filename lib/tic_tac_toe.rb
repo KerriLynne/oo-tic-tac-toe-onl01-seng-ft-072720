e1c858a2361455473995593547fea63109cb91b9
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-06 18:59:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ WIN_COMBINATIONS = [
   end
 
   def position_taken?(index_i)
-    ((@board[sindex_i] == "X") || (@board[index_i] == "O"))
+    ((@board[index_i] == "X") || (@board[index_i] == "O"))
   end
 
   # def self.all
