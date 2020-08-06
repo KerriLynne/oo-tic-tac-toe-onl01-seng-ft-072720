3ba57d949ac64555290f29c51d9770836524feaa
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-06 19:40:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,6 +105,16 @@ WIN_COMBINATIONS = [
     end
   end
 
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+
+
 
 
 
